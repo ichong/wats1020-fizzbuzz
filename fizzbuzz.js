ba6cafe352c6761da6061c25e6407a3b8225c723
chong.isa <chong.isa@gmail.com>
2016-04-04 00:33:48 -0400

    Add JS for Fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,16 @@
-// Place your FizzBuzz code here. 
+// Count through the range limit 1-20 and figure out which numbers are divisible by 3, 5 or both
+var rangeLimit = 20;
+for (var i = 1; i <= rangeLimit; i++) {
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+    console.log('FizzBuzz');
+  }
+  else if (i % 3 === 0) {
+    console.log('Fizz');
+  }
+  else if (i % 5 === 0) {
+    console.log('Buzz');
+  }
+  else {
+    console.log(i);
+  }
+}
